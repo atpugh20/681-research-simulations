e997b75428f48fe23e01cf14ece3ad0c9c8241dd
Alex <133706872+atpugh20@users.noreply.github.com>
2025-03-16 01:36:36 -0700

    Initialized vector class and created test page

---
+++ scripts/test-page/ball.js
@@ -0,0 +1,55 @@
+class Ball {
+    constructor(x = 0, y = 0, radius = 5, color = "white") {
+        this.pos = new Vector(x, y);
+        this.vel = new Vector;
+        this.acc = new Vector(0, 1);
+        this.radius = radius;
+        this.color = color;
+        this.friction = 0.8;
+    }
+
+    update() {
+        /**
+         * Updates the position of the ball based on the velocity
+         * and acceleration attributes.
+         */
+
+        this.vel.addVector(this.acc);
+        this.pos.addVector(this.vel);
+
+        // Handle wall collision  
+        if (this.pos.y > canvas_length - this.radius) {
+            this.pos.y = canvas_length - this.radius; 
+            this.vel.x *= this.friction;
+            this.vel.y *= -this.friction;
+        }
+        if (this.pos.y < this.radius) {
+            this.pos.y = this.radius;
+            this.vel.x *= this.friction;
+            this.vel.y *= -this.friction;
+        }
+        if (this.pos.x > canvas_length - this.radius) {
+            this.pos.x = canvas_length - this.radius; 
+            this.vel.x *= -this.friction;
+            this.vel.y *= this.friction;
+        }
+        if (this.pos.x < this.radius) {
+            this.pos.x = this.radius;
+            this.vel.x *= -this.friction;
+            this.vel.y *= this.friction;
+
+        }
+
+    }
+
+    draw(ctx) {
+        /**
+         * Draws the ball to the canvas using the position, radius,
+         * and color attributes
+         */
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
---
+++ scripts/vector.js
@@ -0,0 +1,76 @@
+class Vector {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(other_x, other_y) {
+        /**
+         * Add other_x to x and other_y to y.
+         */
+        this.x += other_x;
+        this.y += other_y;
+    }
+
+    addVector(other_vector) {
+        /**
+         * Add the passed in vector to the current vector.
+         */
+        this.x += other_vector.x;
+        this.y += other_vector.y;
+    }
+
+    sub(other_x, other_y) {
+        /**
+         * Subtract other_x from x and other_y from y.
+         */
+        this.x -= other_x;
+        this.y -= other_y;
+    }
+
+    subVector(other_vector) {
+        /**
+         * Subtract the passed in vector from the current vector.
+         */
+        this.x -= other_vector.x;
+        this.y -= other_vector.y;
+    }
+
+    mult(scalar) {
+        /**
+         * Multiply both x and y by the scalar argument.
+         */
+        this.x *= scalar;
+        this.y *= scalar;
+    }
+
+    div(scalar) {
+        /**
+         * Divide both x and y by the scalar argument.
+         */
+        if (scalar != 0) {
+            this.x /= scalar;
+            this.y /= scalar;
+        } else {
+            console.log("Cannot divide vector by 0!");
+        }
+    }
+
+    getMag() {
+        /**
+         * Returns the magnitude of the current vector. 
+         * This is the distance from (0, 0) to (x, y).
+         */
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    normalize() {
+        /**
+         * Scales the vector to have a magnitude of 1, resulting in a
+         * unit vector. This new vector is returned in order to keep
+         * the current vector. 
+         */
+        const mag = this.getMag();
+        return new Vector(this.x / mag, this.y / mag);
+    }
+}
