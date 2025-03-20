d80d1c51107e8f43b66217999ff8c9f6c43257f9
Alex <133706872+atpugh20@users.noreply.github.com>
2025-03-19 23:44:19 -0700

    started actual simulation. Finished logic for eular integration

--- scripts/eular-integration/ball.js
+++ scripts/eular-integration/ball.js
@@ -1,45 +1,35 @@
 class Ball {
-    constructor(x = 0, y = 0, radius = 5, color = "white") {
-        this.pos = new Vector(x, y);
-        this.vel = new Vector(0, 0);
-        this.acc = new Vector(0, 9800);
-        this.radius = radius;
-        this.color = color;
-        this.friction = 0.8;
-    }
+  constructor(x = 0, y = 0, radius = 5, color = "white") {
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(0, 0);
+    this.acc = new Vector(0, 9.80665);
+    this.radius = radius;
+    this.color = color;
+    this.grounded = false;
+  }
 
-    update(delta_time) {
-        // Eular method for the numerical integration
-        this.vel.addVector(this.acc.getMult(delta_time));
-        this.pos.addVector(this.vel.getMult(delta_time));
+  update(delta_time) {
+    // Eular method for the numerical integration
+    this.vel.addVector(this.acc.getMult(delta_time));
+    this.pos.addVector(this.vel.getMult(delta_time));
 
-        // Handle wall collision  
-        if (this.pos.y > canvas_length - this.radius) {
-            this.pos.y = canvas_length - this.radius; 
-            this.vel.x *= this.friction;
-            this.vel.y *= -this.friction;
-        }
-        if (this.pos.y < this.radius) {
-            this.pos.y = this.radius;
-            this.vel.x *= this.friction;
-            this.vel.y *= -this.friction;
-        }
-        if (this.pos.x > canvas_length - this.radius) {
-            this.pos.x = canvas_length - this.radius; 
-            this.vel.x *= -this.friction;
-            this.vel.y *= this.friction;
-        }
-        if (this.pos.x < this.radius) {
-            this.pos.x = this.radius;
-            this.vel.x *= -this.friction;
-            this.vel.y *= this.friction;
-        }
+    // Stop ball on ground
+    if (this.pos.y > canvas_length - this.radius) {
+      this.pos.y = canvas_length - this.radius;
+      this.grounded = true;
+      this.color = "red";
+      this.vel.y = 0;
     }
+  }
 
-    draw(ctx) {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
-        ctx.fill();
-    }
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  run_simulations(sim_count) {
+    console.log("Run sim for:", canvas_length - ball.pos.y);
+  }
 }
--- scripts/eular-integration/main.js
+++ scripts/eular-integration/main.js
@@ -1,39 +1,82 @@
-const balls = [];
-const BALL_COUNT = 100;
+const RADIUS = 1;
+const G = 9.80655;
 
-const ball_gap = canvas.width / BALL_COUNT;
+const SIM_TIMES = {};
+let actual_times = {};
+
+const SIM_COUNT = 1000;
+const DISTANCES = [10, 50, 100, 500, 1000];
+let d_iterator = 0;
+
+let reached_ground = false;
+let first_sim = true;
+let running_sims = true;
+let time_hit = 0;
 
 let delta_time;
 let last_frame_time;
 
-function setup() {
-    last_frame_time = 0;
+const ball = new Ball(
+  canvas_length / 2,
+  canvas_length - DISTANCES[0],
+  RADIUS,
+  "white"
+);
 
-    for (let i = 0; i < BALL_COUNT; i++) {
-        balls.push(
-            new Ball((i * ball_gap) + (ball_gap / 2), 0, ball_gap / 2, "white")
-        );
-        balls[i].vel.y = i * 100;
-    }
+function get_times_to_ground(heights, gravity) {
+  const times = {};
+
+  for (let i = 0; i < heights.length; i++) {
+    let h = heights[i];
+    times[h] = Math.sqrt((2 * h) / gravity);
+  }
+
+  return times;
+}
+
+function setup() {
+  last_frame_time = 0;
+  actual_times = get_times_to_ground(DISTANCES, G);
+
+  console.log(actual_times);
 }
 
 function draw(current_time) {
-    clearCanvas();
-
-    delta_time = (current_time - last_frame_time) / 1000;
-    last_frame_time = current_time;
-
-    for (let i = 0; i < balls.length; i++) {
-        balls[i].update(delta_time);
-        balls[i].draw(ctx);
+  clearCanvas();
+  delta_time = (current_time - last_frame_time) / 1000;
+  last_frame_time = current_time;
+  if (running_sims) {
+    if (first_sim) {
+      ball.run_simulations(SIM_COUNT);
+      first_sim = false;
     }
 
-    requestAnimationFrame(draw);
+    ball.update(delta_time);
+    ball.draw(ctx);
+
+    time_hit += delta_time;
+    if (ball.grounded) {
+      SIM_TIMES[DISTANCES[d_iterator]] = time_hit;
+      time_hit = 0;
+      if (d_iterator == DISTANCES.length - 1) {
+        console.log("Finished.");
+        return;
+      } else {
+        d_iterator++;
+        first_sim = true;
+        ball.pos.y = canvas_length - DISTANCES[d_iterator];
+        ball.grounded = false;
+        ball.color = "white";
+      }
+    }
+  }
+
+  requestAnimationFrame(draw);
 }
 
 function main() {
-    setup();
-    requestAnimationFrame(draw);
+  setup();
+  requestAnimationFrame(draw);
 }
 
 main();
